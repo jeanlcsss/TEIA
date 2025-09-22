5af6b7a1f054604de187fb2d28c8c007583759a7
Jean Lucas <jeanlucas2504@gmail.com>
2025-09-22 14:17:02 -0300

    implementando o boxplot pros 2 algoritmos

--- boxplot.py
+++ boxplot.py
@@ -3,7 +3,7 @@ import numpy as np
 import pandas as pd 
 import matplotlib.pyplot as plt
 
-results = np.loadtxt("resultados_estocastico.txt")
+results = np.loadtxt("resultados.txt")
 resultados = results.tolist()
 
 algoritmo  = ['Hill Climbing'] * len(resultados)
@@ -14,4 +14,21 @@ sns.boxplot(df_result, x="algoritmo", y = 'fitness')
 
 sns.boxplot(df_result, x="algoritmo", y = 'fitness')
 sns.swarmplot(df_result, x="algoritmo", y = 'fitness')
+# plt.show()
+
+
+### Analisando 2 algoritmos (agora com estocastico)
+
+results_2 = np.loadtxt("resultados_estocastico.txt")
+resultados_2 = results_2.tolist()
+algoritmo2 = ['Hill Climbing Estocástico'] * len(resultados_2)
+
+df_result2 = pd.DataFrame({'fitness': resultados_2, 'algoritmo' : algoritmo2})
+
+df_final = pd.concat([df_result, df_result2])
+
+df_final = df_final.reset_index(drop=True)
+
+sns.boxplot(df_final, x="algoritmo", y = 'fitness', hue = "algoritmo")
+sns.swarmplot(df_final, x="algoritmo", y = 'fitness', hue = "algoritmo")
 plt.show()
